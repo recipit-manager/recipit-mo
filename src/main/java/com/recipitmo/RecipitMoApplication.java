63412a5282d2fca6a4ddc05f15ca448cc8da9d55
yuncorn <dldbsgh29@gmail.com>
2025-10-30 14:30:53 +0900

    initial commit

---
+++ src/main/java/com/recipitmo/RecipitMoApplication.java
@@ -0,0 +1,13 @@
+package com.recipitmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipitMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipitMoApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+JSP Page Test
+<p>${response.code}</p>
+<p>${response.message}</p>
+<p>${response.data}</p>
+</body>
+</html>
